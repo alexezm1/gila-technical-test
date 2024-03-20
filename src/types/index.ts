6275920a09a9b71fd436decda1406a26739ee33a
alexezm1 <alex_speed_96@hotmail.com>
2024-03-19 17:11:22 -0700

    added global children type and Theme type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
+export type ChildrenProps = {
+    children: React.ReactNode
+}
+
 export type Post = {
     userId: number,
     id: number,
     title: string,
     body: string
 }
+
+export type Theme = 'light' | 'dark'
