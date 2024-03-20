17e7486002e9f82a120be447e002ea501d022e92
alexezm1 <alex_speed_96@hotmail.com>
2024-03-19 17:17:07 -0700

    added theme state as class to apply dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useContext } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { ThemeContext } from "./context/ThemeContext";
 import Home from "./pages/Home";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <>
+    <div className={theme}>
       <Header />
-      <main className="p-3 my-16">
+      <main className="p-3 my-[60px] bg-white dark:bg-slate-600">
         <Home />
       </main>
       <Footer />
-    </>
+      <ToastContainer position="top-right" />
+    </div>
   );
 }
 
