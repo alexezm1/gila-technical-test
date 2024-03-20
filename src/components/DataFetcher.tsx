84f267d7db40032332a958226b82bbae9b887109
alexezm1 <alex_speed_96@hotmail.com>
2024-03-19 17:15:26 -0700

    fetch data function and corrected type errors

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Post } from "../types";
 import PostItem from "./PostItem";
 import Spinner from "./Spinner";
 
 function DataFetcher() {
   const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  // Fetch Data from jsonplaceholder API
-  const fetchData = async () => {
-    setLoading(true);
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await res.json();
-    setPosts(data);
-    setLoading(false);
-  };
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
+    // Fetch Data from jsonplaceholder API
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await res.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch data");
+      }
+    }
     fetchData();
   }, []); // Fetch data once so we leave dependency array empty
 
@@ -27,8 +32,8 @@ function DataFetcher() {
 
   return (
     <div>
-      {posts.map((post, index) => (
-        <PostItem post={post} key={index} />
+      {posts.map((post: Post) => (
+        <PostItem post={post} key={post.id} />
       ))}
     </div>
   );
