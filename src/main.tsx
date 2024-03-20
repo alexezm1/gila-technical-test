222ec73536f408527e1f6d7d71cbc2a1dffbb4fc
alexezm1 <alex_speed_96@hotmail.com>
2024-03-19 17:12:25 -0700

    wrapped app component around Theme Provider to get global state

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
